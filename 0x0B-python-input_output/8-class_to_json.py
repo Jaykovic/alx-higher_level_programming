d07f1a5d0710eee3017b81ffc70318b7c1b35c10
James Obekpa <82936004+Jaykovic@users.noreply.github.com>
2022-07-05 10:11:11 +0100

    updated

--- 0x0B-python-input_output/8-class_to_json.py
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-# 10-class_to_json.py
-# Brennan D Baraban <375@holbertonschool.com>
+# 8-class_to_json.py
+# Auth: James Obekpa
 """Defines a Python class-to-JSON function."""
 
 
