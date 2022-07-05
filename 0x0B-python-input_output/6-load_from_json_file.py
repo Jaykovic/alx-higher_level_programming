d034325e409f0df0a9216f5e8ee6a16134e70ffb
James Obekpa <82936004+Jaykovic@users.noreply.github.com>
2022-07-05 10:09:13 +0100

    updated

--- 0x0B-python-input_output/6-load_from_json_file.py
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-# 8-load_from_json_file.py
-# Brennan D Baraban <375@holbertonschool.com>
+# 6-load_from_json_file.py
+# Auth: James Obekpa
 """Defines a JSON file-reading function."""
 import json
 
