3a7dee3c5d107c44f6157bd9a58e489f8283a8ba
James Obekpa <82936004+Jaykovic@users.noreply.github.com>
2022-07-05 10:10:03 +0100

    updated

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-# 9-add_item.py
-# Brennan D Baraban <375@holbertonschool.com>
+# 7-add_item.py
+# Auth: James Obekpa
 """Add all arguments to a Python list and save them to a file."""
 import sys
 
